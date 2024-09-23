dbf360e11492590c72ad37aba4f451f12c9eced7
ranaadeel15911 <ra0511083@gmail.com>
2024-09-23 16:02:43 +0500

    change

--- model/db.js
+++ model/db.js
@@ -1,8 +1,13 @@
 let mongoose = require('mongoose');
 
+const MONGODB_URI = 'mongodb+srv://sharjeelakhtar245:sharjeel3322@cluster0.ssd7alz.mongodb.net/yourDatabaseName';
+
 try {
-    let connect = mongoose.connect(process.env.MONGODB_URI);
-    console.log("db connected")
+    let connect = mongoose.connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    console.log("db connected");
 } catch (e) {
-    console.log(e)
+    console.log("Error connecting to MongoDB:", e);
 }
